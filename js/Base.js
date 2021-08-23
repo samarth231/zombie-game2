b42a5f485fef9f7e3cd672be7b8044fbf34a2fdd
samarth231 <68042711+samarth231@users.noreply.github.com>
2021-08-22 19:42:17 -0500

    Add files via upload

---
+++ js/Base.js
@@ -0,0 +1,23 @@
+class Base{
+
+    constructor(x,y,w,h){
+        var options = {
+            isStatic:true
+        }
+        this.w = w
+        this.h = h
+        this.body = Bodies.rectangle(x,y,w,h,options)
+        World.add(world,this.body)
+    }
+    show() {
+        var pos = this.body.position;
+        push ();
+        rectMode(CENTER);
+        rect(pos.x,pos.y, this.w, this.h);
+        pop ();
+
+    }
+
+
+
+}
